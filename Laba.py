99fa2a1c7d4f01f2f4bc4b3cc2d5ecb5761280eb
OneShao <farvazovairat@gmail.com>
2023-12-08 14:29:46 +0500

    добавление кнопок

--- Laba.py
+++ Laba.py
@@ -1,3 +1,22 @@
 ﻿import tkinter as tk
 from tkinter import Label, ttk
 from tkinter import messagebox
+
+#Создание окна
+root = tk.Tk()
+root.title("Кальулятор мебели")
+
+#Кнопка для расчета
+Result_btn = ttk.Button(root, text = "Рассчитать")
+Result_btn.grid(row=5, columnspan=2)
+
+#результат
+global result_label
+result_label = ttk.Label(root,text="Себестоимость: 0 рублей")
+result_label.grid(row=6,columnspan=2)
+
+#кнопка выхода
+exit_button = ttk.Button(root,text = "Выход",command=root.quit)
+exit_button.grid(row=7,columnspan=2)
+
+root.mainloop()
